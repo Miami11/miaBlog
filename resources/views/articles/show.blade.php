34a828d877ca595eea7b0fcf9e861210e7495c11
mia chen <ya.ting.chen110@gmail.com>
2017-12-07 18:21:25 +0800

    fix msg in view

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -7,13 +7,20 @@
         <div class="jumbotron">
             <h1>{{ $article->title }}</h1>
             <p class="lead">{{ $article->description }}</p>
-            <a class="btn btn-primary" role="button"  href="{{ route('articles.edit', $article->id) }}">edit</a>
+            <a class="btn btn-primary" role="button" href="{{ route('articles.edit', $article->id) }}">edit</a>
             <form action="{{ route('articles.destroy', $article->id) }}" method="post">
                 {{ csrf_field() }}
                 {{ method_field('delete') }}
                 <button class="btn btn-md btn-danger" type="submit">Delete</button>
             </form>
-
+            Message::
+            @foreach($article->comments()->get() as $msg)
+                <h5>{{ $msg->text }}</h5>
+            <h6>Tag::</h6>
+                @foreach($msg->tags()->get() as $tag)
+                    <span>#{{ $tag->name }}</span>
+                @endforeach
+            @endforeach
         </div>
         <div class="row">
             <div class="col-lg-4">
